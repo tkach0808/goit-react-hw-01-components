d44dc1f4e8b4b2ed8f8923d33a4a14ddd87da760
tkach0808 <tkach0808@gmail.com>
2020-09-19 19:29:23 +0300

    Updates

---
+++ precache-manifest.6d1dc76fb6318fd804daf07ecd98b7cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4080990b4cb421d3f9556199c4ba9bfd",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "20c7ea85d48799774f8f",
+    "url": "/goit-react-hw-01-components/static/css/main.7682f797.chunk.css"
+  },
+  {
+    "revision": "8702e87f2de9d9d43916",
+    "url": "/goit-react-hw-01-components/static/js/2.f39d56b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.f39d56b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20c7ea85d48799774f8f",
+    "url": "/goit-react-hw-01-components/static/js/main.0e21f83c.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
